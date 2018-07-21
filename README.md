4758b9374ae92d99e48fb53626dae388b5696ddd
duyanpeng <duyanpeng@duyanpengdeMacBook-Pro.local>
2018-07-21 10:38:11 +0800

    打包发布及更新文档

--- README.md
+++ README.md
@@ -1,2 +1,180 @@
-# vue-quick-loadmore
+# vue-simple-picker
+[![Build Status](https://travis-ci.org/duyanpeng/vue-simple-picker.svg?branch=master)](https://travis-ci.org/duyanpeng/vue-simple-picker)
+![Read the Docs](https://img.shields.io/readthedocs/pip.svg)
+
 基于vue的移动端下拉刷新上拉无限滚动加载的高度定制化插件
+
+<img src="./static/zhengzaishuaxin.jpg" width="200"/>
+<img src="./static/shifangshuaxin.jpg" width="200"/>
+<img src="./static/zhengzaishuaxin.jpg" width="200"/>
+<img src="./static/shanglajiazai.jpg" width="200"/>
+<img src="./static/wugengduoshuju.jpg" width="200"/>
+
+
+## 安装
+---
+`npm install vue-quick-loadmore -S`
+
+```
+import Vue from 'vue';
+import VueQuickLoadmore from 'vue-quick-loadmore';
+import 'vue-quick-loadmore/dist/index.css';  // 引入插件相关的css
+
+Vue.use(VueQuickLoadmore)
+```
+
+## 使用
+---
+```html
+<template>
+    <div id="app">
+        <quick-loadmore :top-method="handleTop" ref="vueLoad" :top-status-change="handleStatusChange" :bottom-method="handleBottom" :bottom-status-change="handleBottomStatusChange" :disable-top="false" :disable-bottom="false">
+            <div class="item" v-for="(item,index) in        dataList" :key="index">{{index}}</div>
+        </quick-loadmore>
+    </div>
+</template>
+```
+```javascript
+export default {
+  name: "Message",
+  data() {
+    return {
+      dataList: [1, 2, 3, 4, 5]
+    };
+  },
+  // 实现进入详情页返回列表位置功能-搭配keep-alive
+  beforeRouteLeave(to, from, next) {
+    // 进入详情
+    if (to.name === "MessageDetail") {
+        // 获得滚动距离
+      let scrollTop = this.$refs.vueLoad.getScrollTop();
+      // 设置缓存
+      sessionStorage.setItem("messageScrollTop", scrollTop);
+    } else {
+      // 如果去其他页移除缓存  
+      sessionStorage.removeItem("messageScrollTop");
+    }
+    next();
+  },
+  activated() {
+    // 激活路由  
+    let scrollTop = sessionStorage.getItem("messageScrollTop");
+    // 判断来源
+    if (scrollTop == null) {
+      console.log("不需要缓存的页面");
+    } else {
+     // 需要缓存的页面,滚动
+      this.$refs.vueLoad.setScrollTop(scrollTop);
+    }
+  },
+  deactivated() {
+
+  },
+  methods: {
+    handleTop() {
+      // 下拉刷新  
+      setTimeout(() => {
+        this.dataList = [1, 2, 3, 4, 5];
+        // 刷新成功调用
+        this.$refs.vueLoad.onTopLoaded();
+      }, 1000);
+    },
+    handleStatusChange(status) {
+      // status监控下拉刷新状态--等待/下拉/到达阙值/刷新
+      const TOPSTATUS = {
+        wait: "wait",
+        pulling: "pulling",
+        limit: "limit",
+        loading: "loading"
+      }; 
+      console.log(status, "statuschange");
+    },
+    handleBottomStatusChange(status) {
+      // status监控上拉加载状态--等待/加载/没有更多数据  
+      const BOTTOMSTATUS = {
+        wait: "wait",
+        loading: "loading",
+        nodata: "nodata"
+      };
+      console.log(status, "bottomchange");
+    },
+    handleBottom() {
+      // 上拉加载
+      setTimeout(() => {
+        this.dataList.push(1, 2, 3);
+        // 加载完毕调用函数(如果没有更多数据参数传false)
+        this.$refs.vueLoad.onBottomLoaded();
+      }, 1000);
+    }
+  },
+  mounted() {
+    // console.log("mounted");
+  }
+};
+```
+```css
+*{
+    margin:0;
+    padding:0;
+}
+html,body,#app{
+    height:100%;
+    overflow: hidden;
+}
+.item{
+    height:200px;
+    background: lightgray;
+    border:2px solid green;
+    overflow: hidden;
+}
+```
+
+## 下拉刷新配置
+---
+参数|类型|说明|备注
+:--:|:--:|:--:|:--:
+v-bind:disableTop|Boolean|禁止下拉刷新|默认:false
+v-bind:distanceIndex|Number|手指滑动与页面滑动比例|默认:2
+v-bind:topLoadingDistance|Number|loading状态时页面距顶部距离|默认：50
+v-bind:topDistance|Number|下拉刷新触发值|默认:100
+v-on:topMethod|Function|下拉刷新触发方法|
+v-on:topStatusChange|Function|下拉刷新状态改变回调|四种状态具体见示例代码
+ref.onTopLoaded|Function|下拉刷新完成时调用函数|通过ref使用具体见示例代码
+---
+### 上拉加载配置:
+参数|类型|说明|备注
+:--:|:--:|:--:|:--:
+v-bind:disableBttom|Boolean|禁止上拉加载|默认:false
+v-bind:bottomDistance|Number|上拉加载触发值|默认:10
+v-on:bottomMethod|Function|上拉加载触发方法|
+v-on:bottomStatusChange|Function|上拉加载状态改变回调|三种状态具体见示例代码
+ref.onBottomLoaded(boolean = true)|Function|上拉加载完成时调用函数|通过ref使用具体见示例代码(注意：如果下拉加载之后是无更多数据状态,函数传参为false,此后不在触发上拉刷新方法,下拉刷新之后会自动开启上拉加载)
+---
+### 其他配置:
+参数|类型|说明|备注
+:--:|:--:|:--:|:--:
+ref.getScrollTop|Function|获得滚动距离|具体见示例代码
+ref.setScroolTop(y)|Function|设置滚动距离|具体见示例代码
+---
+### 替换下拉刷新下拉加载loading图方法:
+
+```html
+<template slot="top">
+    <div>根据topStatusChange返回的状态,渲染下拉刷新不同阶段的样式</div>
+</template>    
+<template slot="bottom">
+    <div>根据bottomStatusChange返回的状态,渲染上拉加载不同阶段的样式</div>
+</template>   
+
+```
+
+## 版本更新说明
+---
+版本号|说明|
+:--:|:--:|
+
+0.0.1|项目初始化
+
+## 联系我
+---
+QQ:215028726
---
+++ dist/index.js
@@ -0,0 +1,444 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"Gqfg":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+var TOPSTATUS = {
+  wait: "wait",
+  pulling: "pulling",
+  limit: "limit",
+  loading: "loading"
+};
+var BOTTOMSTATUS = {
+  wait: "wait",
+  loading: "loading",
+  nodata: "nodata"
+};
+exports.default = {
+  props: {
+    // 禁止上拉刷新
+    disableTop: {
+      type: Boolean,
+      default: false
+    },
+    // 上拉移动比例
+    distanceIndex: {
+      type: Number,
+      default: 2
+    },
+    // loading时与顶端距离
+    topLoadingDistance: {
+      type: Number,
+      default: 50
+    },
+    // 上拉距离触发值
+    topDistance: {
+      type: Number,
+      default: 100
+    },
+    // 上拉触发方法
+    topMethod: {
+      type: Function,
+      default: function _default() {
+        return function () {
+          console.log("topmethod");
+        };
+      }
+    },
+    // 上拉刷新状态改变
+    topStatusChange: {
+      type: Function,
+      default: function _default() {
+        return function () {
+          console.log("topStatusChange");
+        };
+      }
+    },
+
+    // 禁止下拉加载
+    disableBottom: {
+      type: Boolean,
+      default: false
+    },
+    // 触发下拉加载距离
+    bottomDistance: {
+      type: Number,
+      default: 10
+    },
+    // 下拉加载方法
+    bottomMethod: {
+      type: Function,
+      default: function _default() {
+        return function () {};
+      }
+    },
+    // 下拉刷新状态改变
+    bottomStatusChange: {
+      type: Function,
+      default: function _default() {
+        return function () {
+          console.log("topStatusChange");
+        };
+      }
+    }
+  },
+  data: function data() {
+    return {
+      startPositionTop: null,
+      startScreenY: 0,
+      endScreenY: 0,
+      topStatus: TOPSTATUS.wait, // 'wait'等待 , 'pulling'下拉,'limit'超过topDistance触发,'loading'正在loading
+      bottomOverflow: "auto",
+      bottomStatus: BOTTOMSTATUS.wait
+    };
+  },
+
+
+  components: {},
+
+  computed: {
+    topText: function topText() {
+      switch (this.topStatus) {
+        case TOPSTATUS.pulling:
+          return "下拉刷新";
+          break;
+        case TOPSTATUS.limit:
+          return "释放刷新";
+          break;
+        case TOPSTATUS.loading:
+          return "正在刷新...";
+          break;
+        default:
+          return "";
+      }
+    },
+    bottomText: function bottomText() {
+      switch (this.bottomStatus) {
+        case BOTTOMSTATUS.loading:
+          return "正在加载更多...";
+          break;
+        case BOTTOMSTATUS.nodata:
+          return "暂无更多数据";
+          break;
+        default:
+          return "";
+      }
+    }
+  },
+  watch: {
+    topStatus: function topStatus(next) {
+      this.topStatusChange(next);
+    },
+    bottomStatus: function bottomStatus(next) {
+      this.bottomStatusChange(next);
+    }
+  },
+  mounted: function mounted() {
+    this.init();
+  },
+
+
+  methods: {
+    handleScroll: function handleScroll() {
+      var _this = this;
+
+      if (this.disableBottom) {
+        return;
+      }
+      if (this.bottomStatus !== BOTTOMSTATUS.wait) {
+        return;
+      }
+
+      var bDistance = this.$el.scrollHeight - this.$el.scrollTop - this.$el.clientHeight;
+      if (bDistance <= this.bottomDistance) {
+        this.bottomStatus = BOTTOMSTATUS.loading;
+        this.$nextTick(function () {
+          _this.$el.scrollTo(0, _this.$el.scrollHeight);
+          // this.$el.scrollTop = this.$el.scrollHeight
+        });
+        this.bottomMethod();
+      }
+    },
+
+    // 获得滚动距离
+    getScrollTop: function getScrollTop() {
+      return this.$el.scrollTop;
+    },
+
+    // 设置滚动距离
+    setScrollTop: function setScrollTop(y) {
+      var _this2 = this;
+
+      this.$nextTick(function () {
+        _this2.$el.scrollTop = parseFloat(y);
+      });
+    },
+    init: function init() {
+      this.startPositionTop = this.$refs.content.getBoundingClientRect().top;
+      if (!this.disableTop) {
+        this.bindTouchEvents();
+      }
+    },
+    bindTouchEvents: function bindTouchEvents() {
+      this.$refs.content.addEventListener("touchstart", this.handleTouchStart);
+      this.$refs.content.addEventListener("touchmove", this.handleTouchMove);
+      this.$refs.content.addEventListener("touchend", this.handleTouchEnd);
+    },
+    handleTouchStart: function handleTouchStart(e) {
+      // 只有没滚动时才触发事件
+      if (this.$refs.content.getBoundingClientRect().top < this.startPositionTop) {
+        return;
+      }
+      if (this.topStatus === TOPSTATUS.loading) {
+        return;
+      }
+
+      var screenY = e.touches[0].screenY;
+      this.startScreenY = screenY;
+    },
+    handleTouchMove: function handleTouchMove(e) {
+      // 只有没滚动时才触发事件
+      if (this.$refs.content.getBoundingClientRect().top < this.startPositionTop) {
+        return;
+      }
+      if (this.topStatus === "loading") {
+        return;
+      }
+
+      var screenY = e.touches[0].screenY;
+      this.endScreenY = screenY;
+      var moveDistance = (screenY - this.startScreenY) / this.distanceIndex;
+      if (this.$refs.content.getBoundingClientRect().top > this.startPositionTop) {
+        this.topStatus = TOPSTATUS.pulling;
+      }
+
+      if (moveDistance >= this.topDistance) {
+        this.topStatus = TOPSTATUS.limit;
+      }
+      if (moveDistance > 0) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        this.transformStyle(this.$refs.content, moveDistance);
+      }
+    },
+    handleTouchEnd: function handleTouchEnd(e) {
+      // 只有没滚动时才触发事件
+      if (this.$refs.content.getBoundingClientRect().top < this.startPositionTop) {
+        return;
+      }
+      if (this.topStatus === "loading") {
+        return;
+      }
+
+      if ((this.endScreenY - this.startScreenY) / this.distanceIndex >= this.topDistance) {
+        this.transformStyle(this.$refs.content, this.topLoadingDistance, true);
+        this.topStatus = TOPSTATUS.loading;
+        this.topMethod();
+        if (!this.disableBottom) {
+          this.bottomStatus = BOTTOMSTATUS.wait;
+        }
+      } else {
+        this.topStatus = TOPSTATUS.wait;
+        this.transformStyle(this.$refs.content, 0);
+        this.startScreenY = 0;
+        this.endScreenY = 0;
+      }
+    },
+
+    // 上拉数据加载完
+    onTopLoaded: function onTopLoaded() {
+      this.transformStyle(this.$refs.content, 0, true);
+      this.topStatus = TOPSTATUS.wait;
+      this.startScreenY = 0;
+      this.endScreenY = 0;
+    },
+
+    // 下拉数据加载完
+    onBottomLoaded: function onBottomLoaded() {
+      var flag = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
+
+      if (flag) {
+        this.bottomStatus = BOTTOMSTATUS.wait;
+      } else {
+        this.bottomStatus = BOTTOMSTATUS.nodata;
+      }
+    },
+
+    // 动画
+    transformStyle: function transformStyle(target, moveDistance, transition) {
+      var timer = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 200;
+
+      target.style["-webkit-transform"] = "translate3d(0," + moveDistance + "px,0)";
+      target.style["transform"] = "translate3d(0," + moveDistance + "px,0)";
+      target.style.transitionDuration = "0ms";
+      if (transition) {
+        target.style.transitionDuration = timer + "ms";
+      }
+    }
+  }
+};
+        var $44ea29 = exports.default || module.exports;
+      
+      if (typeof $44ea29 === 'function') {
+        $44ea29 = $44ea29.options;
+      }
+    
+        /* template */
+        Object.assign($44ea29, (function () {
+          var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"vsim-load",style:({overflow:_vm.bottomOverflow}),on:{"&scroll":function($event){return _vm.handleScroll($event)}}},[_c('div',{ref:"content",staticClass:"vsim-load-content"},[_vm._t("top",[_c('div',{staticClass:"vsim-load-header"},[_c('div',[_vm._v(_vm._s(_vm.topText))])])]),_vm._v(" "),_vm._t("default"),_vm._v(" "),_vm._t("bottom",[_c('div',{staticClass:"vsim-load-footer"},[_c('div',[_vm._v(_vm._s(_vm.bottomText))])])])],2)])}
+var staticRenderFns = []
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: "data-v-44ea29",
+            functional: undefined
+          };
+        })());
+      
+},{}],"Focm":[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _VueQuickLoadmore = require('./src/VueQuickLoadmore.vue');
+
+var _VueQuickLoadmore2 = _interopRequireDefault(_VueQuickLoadmore);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (Vue) {
+    Vue.component('quick-loadmore', _VueQuickLoadmore2.default);
+};
+},{"./src/VueQuickLoadmore.vue":"Gqfg"}]},{},["Focm"], null)
+//# sourceMappingURL=/index.map
